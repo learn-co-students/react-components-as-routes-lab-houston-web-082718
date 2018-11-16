1ee61851a7b63a5c2c0bf0a993a6c2518761aa4e
spraguesy <andrewsprague@outlook.com>
2018-11-16 13:51:55 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { actors } from '../data';
+import React, { Fragment } from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div>
+            <h1>Name: {actor.name}</h1>
+            <p>Genres:</p>
+            <ul>
+              {actor.movies.map(movie => {
+                return <li>{movie}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+      }
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import Actors from "../components/Actors";
+import Directors from "../components/Directors";
+import Movies from "../components/Movies";
 
-
-const App = (props) => {
+const App = props => {
   return (
     <Router>
-      {/*{code here}*/}
+      <Fragment>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={Movies} />
+        <Route exact path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </Fragment>
     </Router>
   );
 };
 
-export default App
+export default App;
